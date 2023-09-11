b33685edb22317db5b6fc9dc3e92af250974c1aa
Pham Minh Khang <pham.m.khang.98@gmail.com>
2023-09-12 02:34:41 +0700

    .

--- src/components/SideBarItem/SideBarItem.jsx
+++ src/components/SideBarItem/SideBarItem.jsx
@@ -5,8 +5,23 @@ import icons from '../../utils/icons';
 
 const { MdOutlineKeyboardArrowRight } = icons;
 const SideBarItem = ({ category, fillterPrice, fillterAcreage, memoizedCategories }) => {
-  const { prices } = useSelector((state) => state.price);
-  const { acreages } = useSelector((state) => state.acreage);
+  const { prices, acreages } = useSelector((state) => ({
+    prices: state.price.prices,
+    acreages: state.acreage.acreages,
+  }));
+
+  const formatContent = (content) => {
+    if (!Array.isArray(content)) {
+      return [];
+    }
+    const formatContent = [];
+    for (let i = 0; i < content.length; i += 2) {
+      const left = content[i];
+      const right = content[i + 1] !== undefined ? content[i + 1] : null;
+      formatContent.push({ left, right });
+    }
+    return formatContent;
+  };
 
   return (
     <div className="w-full h-fit p-5 flex flex-col bg-white shadow-lg rounded-lg gap-3">
@@ -26,21 +41,41 @@ const SideBarItem = ({ category, fillterPrice, fillterAcreage, memoizedCategorie
           ))}
       </div>
       {fillterPrice &&
-        prices.map((item) => (
-          <div key={item?.code} className="flex items-center text-sm gap-3 border-b border-dashed border-gray-200 pb-1">
-            <MdOutlineKeyboardArrowRight size={18} />
-            <Link className={'transition-all hover:text-red-500 hover:translate-x-2'} to={''}>
-              {item?.value}
-            </Link>
+        formatContent(prices).map((item, index) => (
+          <div key={index} className="">
+            <div className=" flex items-center justify-around gap-2">
+              <div className="flex flex-1 items-center text-sm gap-1 border-b border-dashed border-gray-200 pb-1">
+                <MdOutlineKeyboardArrowRight size={18} />
+                <Link to={''} className="transition-all hover:text-red-500 hover:translate-x-2">
+                  {item.left.value}
+                </Link>
+              </div>
+              <div className="flex flex-1 items-center text-sm gap-1 border-b border-dashed border-gray-200 pb-1">
+                <MdOutlineKeyboardArrowRight size={18} />
+                <Link to="" className="transition-all hover:text-red-500 hover:translate-x-2">
+                  {item.right.value}
+                </Link>
+              </div>
+            </div>
           </div>
         ))}
       {fillterAcreage &&
-        acreages.map((item) => (
-          <div key={item?.code} className="flex items-center text-sm gap-3 border-b border-dashed border-gray-200 pb-1">
-            <MdOutlineKeyboardArrowRight size={18} />
-            <Link className={'transition-all hover:text-red-500 hover:translate-x-2'} to={''}>
-              {item?.value}
-            </Link>
+        formatContent(acreages).map((item, index) => (
+          <div key={index} className="">
+            <div className=" flex items-center justify-around gap-2">
+              <div className="flex flex-1 items-center text-sm gap-1 border-b border-dashed border-gray-200 pb-1">
+                <MdOutlineKeyboardArrowRight size={18} />
+                <Link to={''} className="transition-all hover:text-red-500 hover:translate-x-2">
+                  {item.left.value}
+                </Link>
+              </div>
+              <div className="flex flex-1 items-center text-sm gap-1 border-b border-dashed border-gray-200 pb-1">
+                <MdOutlineKeyboardArrowRight size={18} />
+                <Link to="" className="transition-all hover:text-red-500 hover:translate-x-2">
+                  {item.right.value}
+                </Link>
+              </div>
+            </div>
           </div>
         ))}
     </div>
